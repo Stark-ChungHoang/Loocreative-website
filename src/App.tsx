c8168c944161df1c0425fdfe3827a4fd8420930b
dovanthao2404 <dovanthao2404@gmail.com>
2022-01-22 14:15:35 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import {
   PageFive,
 } from "./pages/index";
 import NavBar from "./components/NavBar";
+import MenuNav from "./components/MenuNav";
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
 
@@ -82,58 +83,7 @@ function App() {
       });
     });
   }, []);
-  const handleActiveMenuNav = (dataPage: string) => {
-    const listNav = document.querySelectorAll<HTMLElement>(".menu_nav > div");
-    const listNavArray = Array.from(listNav);
-    listNavArray.forEach((item) => {
-      if (item.getAttribute("data-page") === dataPage) {
-        item.classList.add("active");
-      } else {
-        item.classList.remove("active");
-      }
-    });
-  };
 
-  const handleScroll = (e: Event) => {
-    const scrollY = window.scrollY;
-    const pageTwoOffset =
-      document.querySelector<HTMLElement>("#page-two")?.offsetTop;
-    const pageThreeOffset =
-      document.querySelector<HTMLElement>("#page-three")?.offsetTop;
-    const pageFourOffset =
-      document.querySelector<HTMLElement>("#page-four")?.offsetTop;
-    const pageFiveOffset =
-      document.querySelector<HTMLElement>("#page-five")?.offsetTop;
-    if (pageTwoOffset && pageThreeOffset && pageFourOffset && pageFiveOffset) {
-      if (scrollY < pageTwoOffset) {
-        window.location.hash = "#firstPage";
-        handleActiveMenuNav("firstPage");
-      } else if (scrollY < pageThreeOffset) {
-        handleActiveMenuNav("secondPage");
-        window.location.hash = "#secondPage";
-      } else if (scrollY < pageFourOffset) {
-        window.location.hash = "#thirdPage";
-        handleActiveMenuNav("thirdPage");
-      } else if (scrollY < pageFiveOffset) {
-        handleActiveMenuNav("fourthPage");
-        window.location.hash = "#fourthPage";
-      } else if (scrollY >= pageFiveOffset) {
-        handleActiveMenuNav("fifthPage");
-        window.location.hash = "#fifthPage";
-      }
-    }
-  };
-  const handleScrollMemo = useMemo(() => handleScroll, []);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScrollMemo);
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      window.removeEventListener("scroll", handleScrollMemo);
-    };
-  }, []);
 
   return (
     <>
@@ -152,69 +102,7 @@ function App() {
       <section id="page-five">
         <PageFive />
       </section>
-      <nav className="menu_nav">
-        <div
-          data-page="firstPage"
-          onClick={() => {
-            window.scroll(0, 0);
-            handleActiveMenuNav("firstPage");
-          }}
-        >
-          {" "}
-        </div>
-        <div
-          data-page="secondPage"
-          onClick={() => {
-            const pageTwoOffset =
-              document.querySelector<HTMLElement>("#page-two")?.offsetTop;
-            if (pageTwoOffset) {
-              window.scroll(0, pageTwoOffset);
-              handleActiveMenuNav("secondPage");
-            }
-          }}
-        >
-          {" "}
-        </div>
-        <div
-          data-page="thirdPage"
-          onClick={() => {
-            const pageThreeOffset =
-              document.querySelector<HTMLElement>("#page-three")?.offsetTop;
-            if (pageThreeOffset) {
-              window.scroll(0, pageThreeOffset);
-              handleActiveMenuNav("thirdPage");
-            }
-          }}
-        >
-          {" "}
-        </div>
-        <div
-          data-page="fourthPage"
-          onClick={() => {
-            const pageFourOffset =
-              document.querySelector<HTMLElement>("#page-four")?.offsetTop;
-            if (pageFourOffset) {
-              window.scroll(0, pageFourOffset);
-              handleActiveMenuNav("fourthPage");
-            }
-          }}
-        >
-          {" "}
-        </div>
-        <div
-          data-page="fifthPage"
-          onClick={() => {
-            const pageFiveOffset =
-              document.querySelector<HTMLElement>("#page-five")?.offsetTop;
-            if (pageFiveOffset) {
-              window.scroll(0, pageFiveOffset);
-              handleActiveMenuNav("fifthPage");
-            }
-          }}
-        >
-          {" "}
-        </div>
-      </nav>
+      <MenuNav />
 
       <NavBar />
     </>
