e63170b0e6e0c14da7f5d37c33e3f3a99f438b5f
dovanthao2404 <dovanthao2404@gmail.com>
2022-01-24 16:54:54 +0700

    Teams page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 
 const Home = lazy(() => import("../pages/Home/index"));
 const Contact = lazy(() => import("../pages/Contanct"));
+const Teams = lazy(() => import("../pages/Teams"));
 const router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/teams" element={<Teams />} />
     </Routes>
   );
 };
