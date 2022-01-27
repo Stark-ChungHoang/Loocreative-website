01eef85d2a97403dda60a1f572c26963399066d1
dovanthao2404 <dovanthao2404@gmail.com>
2022-01-27 14:54:56 +0700

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,166 +1,54 @@
+import "fullpage.js/vendors/scrolloverflow";
 import { useEffect, useState } from "react";
 import PageOne from "./PageOne/PageOne";
 import PageTwo from "./PageTwo/PageTwo";
 import PageThree from "./PageThree/PageThree";
 import PageFourth from "./PageFourth/PageFourth";
 import PageFive from "./PageFive/PageFive";
-import { gsap } from "gsap";
-import { useNavigate } from "react-router-dom";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(ScrollToPlugin);
+import ReactFullpage from "@fullpage/react-fullpage";
+
+
+
+const fullpageOptions = {
+  anchors: ["firstPage", "secondPage", "thirdPage"],
+  sectionsColor: ["#282c34", "#ff5f45", "#0798ec"],
+  callbacks: ["onLeave", "afterLoad"],
+  scrollOverflow: true
+};
 
 function Home() {
-  // set change URL khen scrolling
-  let navigate = useNavigate();
-  const [activeUrl, setActiveUrl] = useState<Number>(0);
-  const handleUrl = () => {
-    switch (activeUrl) {
-      case 0:
-        navigate("#firstPage");
-        break;
-      case 1:
-        navigate("#secondPage");
-        break;
-      case 2:
-        navigate("#thirdPage");
-        break;
-      case 3:
-        navigate("#fourthPage");
-        break;
-      case 4:
-        navigate("#fifthPage");
-        break;
-    }
-  };
-  useEffect(() => {
-    handleUrl();
-  }, [activeUrl]);
-
-  //set up animation scroll to section
-  useEffect(() => {
-    var keys = { 37: 1, 38: 1, 39: 1, 40: 1 } as any;
-    var supportsPassive = false;
-
-    function preventDefault(e: any) {
-      e.preventDefault();
-    }
-    var wheelOpt = supportsPassive ? { passive: false } : false as any;
-
-    function preventDefaultForScrollKeys(e: any) {
-      if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-      }
-    }
-    var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel' as any;
-
-    function disableScroll() {
-      window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-      window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-      window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-      window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-    }
-
-    function enableScroll() {
-      window.removeEventListener('DOMMouseScroll', preventDefault, false);
-      window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-      window.removeEventListener('touchmove', preventDefault, wheelOpt);
-      window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-    }
-    var settingUp = true;
-    var buttonClicked = false;
-
-    function goToSection(i: number) {
-      gsap.set("body", { overflow: "hidden" });
-
-      gsap.to("body", {
-        scrollTo: { y: i * window.innerHeight, autoKill: true },
-        duration: 0.5,
-        onComplete: () => {
-          gsap.set("body", { overflow: "auto" });
-          buttonClicked = false;
-          setActiveUrl(i);
-        },
-      });
-    }
-    const instances = [];
-    gsap.utils.toArray("section").forEach((panel: any, i: any) => {
-      instances.push(
-        ScrollTrigger.create({
-          trigger: panel,
-          onEnter: () => {
-            if (!buttonClicked && !settingUp) {
-              goToSection(i);
-              disableScroll();
-            }
-          },
-          onLeave: () => {
-            enableScroll();
-          },
-          onLeaveBack: () => {
-            enableScroll();
-          }
-        })
-      );
-
-      instances.push(
-        ScrollTrigger.create({
-          trigger: panel,
-          start: "bottom bottom",
-          onEnterBack: () => {
-            if (!buttonClicked && !settingUp) {
-              disableScroll();
-              goToSection(i);
-            }
-          },
-        })
-      );
-    });
-    // set up function for menu_nav
-    gsap.utils.toArray(".buttonNav").forEach((btn: any, i: any) => {
-      btn.onclick = () => {
-        if (!buttonClicked) {
-          buttonClicked = true;
-          goToSection(i);
-        }
-      };
-    });
-    settingUp = false;
-    return (() => { buttonClicked = true; });
-  }, []);
-
-  //variable for array menu_nav
-  let array: any[];
-  array = Array(5).fill(0);
   return (
-    <>
-      <section>
-        <PageOne />
-      </section>
-      <section>
-        <PageTwo />
-      </section>
-      <section>
-        <PageThree />
-      </section>
-      <section>
-        <PageFourth />
-      </section>
-      <section>
-        <PageFive />
-      </section>
-      <nav className="menu_nav">
-        {array.map((item, index) => (
-          <button
-            key={index + item}
-            className={`${activeUrl === index ? "active buttonNav" : "buttonNav"
-              }`}
-          ></button>
-        ))}
-      </nav>
-    </>
+    <ReactFullpage
+      licenseKey={'263DXXXX-B839XXXX-AE67XXXX-F398XXXX'}
+      {...fullpageOptions}
+      scrollOverflow={true}
+      navigation={true}
+      navigationPosition={'right'}
+      normalScrollElements={'.vail'}
+
+      render={({ state, fullpageApi }: { state: any, fullpageApi: any; }) => {
+        return (
+          <div>
+            <section className="section">
+              <PageOne />
+            </section>
+            <section className="section">
+              <PageTwo />
+            </section>
+            <section className="section">
+              <PageThree />
+            </section>
+            <section className="section">
+              <PageFourth />
+            </section>
+            <section className="section">
+              <PageFive />
+            </section>
+
+          </div>
+        );
+      }}
+    />
   );
 }
 
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -14,15 +14,54 @@ const fullpageOptions = {
 const FullpageWrapper = fullpageProps => (
   <ReactFullpage
   licenseKey = {'263DXXXX-B839XXXX-AE67XXXX-F398XXXX'}
-   
+  {...fullpageOptions}
     scrollOverflow={true}
-    sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+    navigation= {true}
+    // slidesNavigation={true}
+
+	navigationPosition={'right'}
+    sectionsColor={["#282c34", "#ff5f45", "#0798ec", "red"]}
+    normalScrollElements={ '.vail'}
+
     render={({ state, fullpageApi }) => {
+      console.log(state,fullpageApi)
       return (
         <div>
-          <div className="section">1</div>
+          <div className="section fp-noscroll">
+            <div className="vail" style={{height: 300, width: 300, overflow:"auto"}} onScrollCapture={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+            }} onScroll={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+            }}
+            onMouseDown={(e) => {
+              console.log(e)
+
+              fullpageApi.setAllowScrolling(false);
+            }}
+            onMouseMove={(e) => {
+
+              // fullpageApi.destroy();
+            }}
+            onMouseLeave={(e) => {
+              fullpageApi.setAllowScrolling(true);
+
+
+            }}
+
+            >
+              <div style={{height: 700, width: 300, background: "red"}}></div>
+            </div>
+          </div>
           <div className="section">
-            <div style={{ height: "150vh",color: "blue" }}>2</div>
+          <div style={{height: 300, width: 300, overflow:"auto"}} onScroll={(e) => {
+              console.log(e)
+              e.preventDefault();
+              e.stopPropagation();
+            }}>
+              <div style={{height: 700, width: 300, background: "red"}}></div>
+            </div>
           </div>
           <div className="section">3</div>
           <div className="section">4</div>
